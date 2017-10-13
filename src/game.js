cbe1cb150c91b1757da500c1d8f8018c14c3a8f4
dustinpfister <dustin.pfister@gmail.com>
2017-10-13 16:12:16 -0400

    setting frame

--- src/game.js
+++ src/game.js
@@ -19,6 +19,7 @@ var Game = (function () {
             k.addKey(65).onHoldCallback = function (key) {
 
                 ship.x -= 1;
+                ship.frame = 2;
 
             };
 
@@ -26,20 +27,29 @@ var Game = (function () {
             k.addKey(68).onHoldCallback = function (key) {
 
                 ship.x += 1;
+                ship.frame = 1;
 
             };
 
+            // W
             k.addKey(87).onHoldCallback = function (key) {
 
                 ship.y -= 1;
 
             };
 
+            // S
             k.addKey(83).onHoldCallback = function (key) {
 
                 ship.y += 1;
 
             };
+
+            k.onUpCallback = function () {
+
+                ship.frame = 0;
+
+            }
         },
 
         create : function () {},
